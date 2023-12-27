8cc4270bdf175a5b092c2f361eab1ba0450521c4
C. Caleb Scott <calebcscott@outlook.com>
2023-12-26 22:25:48 -0600

    Add some additional args for testing

--- src/main.c
+++ src/main.c
@@ -9,9 +9,16 @@
 int main(int argc, char *argv[]) {
     ArgParser argparser;
     argparser_init(&argparser);
+
+    const char *shortH[] = {"-l"};
+    const char *longH[] = {"--long"};
+    argparser_add_optional_arg(&argparser, "long", shortH, 1, longH, 1, "simple description", Arg_Flag );
+
+    const char *ishortH[] = {"-i"};
+    const char *ilongH[] = {"--int"};
+    argparser_add_optional_arg(&argparser, "int", ishortH, 1, ilongH, 1, "simple description", Arg_Value | Arg_Value_Int );
+
     argparser_parse(&argparser, argc, argv);
-
-
     printf("Hello, world!\n");
 
 
