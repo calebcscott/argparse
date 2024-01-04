2309e5ddb435d3f072ada3f7db645fde9d0e8efd
C. Caleb Scott <calebcscott@outlook.com>
2024-01-03 23:00:51 -0600

    Begin adding tests

---
+++ test/Test_ArgParse.c
@@ -0,0 +1,42 @@
+
+
+#include <stdio.h>
+
+
+
+#include "argparse.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+	printf("Hello, Test!\n");
+
+	return 0;
+}
